46bdb57701380926d85593b7d79bcb2292bc1ff2
Angelos Karagiannis <aggeloskar32@gmail.com>
2019-08-26 18:05:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from urllib.request import urlopen
 from os.path import isfile
 from random import choice,randint
 
-
+#downloading file if it isnt already in the folder
 if not isfile('words.txt'):
     print('Downloading words.txt...')
-    url='https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
+    url='https://github.com/dwyl/english-words/raw/master/words.txt'
     with open('words.txt', 'wb') as f:
         f.write(urlopen(url).read())
 
+#open wordlist
 words=open('words.txt','r').read().split("\n")
-special_chars=['!', '?','~', '$']
+special_chars=['!', '?', '~', '$', '&', '%']
 
+
+#generating the password
 def create_password(num_words=1, num_numbers=4, num_special=3):
     pass_str=''
 
@@ -29,11 +32,21 @@ def create_password(num_words=1, num_numbers=4, num_special=3):
 
 
 def main():
-    pass_str=create_password()
-    strength,_=test(pass_str)
 
-    print ('\nPassword: %s' %pass_str)
+    pass1 = input('Enter the password: ')
+    strength,_=test(pass1)
+    print ('\nPassword: %s' %pass1)
     print ('Strength: %0.5f' %strength)
+    if strength<0.7:
+        print ('Consider change your password or use the new and strong password that our Generator created for you')
+        pass_str=create_password()
+        strength,_=test(pass_str)
+        print ('\nPassword: %s' %pass_str)
+        print ('Strength: %0.5f' %strength)
+    
+
+    #SOS ta sxolia sthn main na meinoun alla na dokimasw kai me to input
+
 
 if __name__=='__main__':
     main()
